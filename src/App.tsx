3d7f3d4682c6bd579f91cad90cd0fd2a7456867d
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2024-10-12 14:13:21 -0300

    feat: App component to conditionally render Summary or EmptyGoals based on data availability

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { CreateGoal } from './components/create-goal';
 import { Sheet } from './components/ui/sheet';
 import { EmptyGoals } from './components/empty-goals';
 import { Summary } from './components/summary';
+import { useQuery } from '@tanstack/react-query';
+import { getSummary } from './http/get-summary';
 
 export function App() {
   
+  const {data} = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
+  });
 
   return (
     <Sheet>
-      <EmptyGoals />
-      <Summary />
+      {data && data.total > 0 ? <Summary /> : <EmptyGoals />}
       <CreateGoal />
     </Sheet>
   );
