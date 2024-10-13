44b7995824acba4fdc74ab967d0c335a1a952152
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2024-10-12 23:15:24 -0300

    refactor: Update CreateGoal component to use react-hook-form and zod for form validation

--- src/components/create-goal.tsx
+++ src/components/create-goal.tsx
@@ -1,11 +1,30 @@
 
 import { Button } from './ui/button';
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Input } from './ui/input';
 import { SheetClose, SheetContent, SheetDescription, SheetHeader } from './ui/sheet';
 import { Label } from './ui/label';
 import { RadioGroup, RadioGroupItem } from './ui/radio-group';
+import { z } from 'zod';
+
+const createGoalForm = z.object({
+  title: z.string().min(1,'informe a atividade a realizar'),
+  weeklyFrequency: z.coerce.number().min(1).max(7)
+});
+
+type CreateGoalForm = z.infer<typeof createGoalForm>
+
 
 export function CreateGoal() {
+  const { register, control, handleSubmit, formState } = useForm<CreateGoalForm>({
+    resolver: zodResolver(createGoalForm)
+  });
+
+  function handleCreateGoal(data:CreateGoalForm) {
+    console.log(data);
+  }
+
   return(
     
     <><SheetContent>
@@ -23,53 +42,67 @@ export function CreateGoal() {
           </SheetDescription>
         </div>
 
-        <form action="" className='flex-1 flex flex-col justify-between'>
+        <form onSubmit={handleSubmit(handleCreateGoal)} className='flex-1 flex flex-col justify-between'>
           <div className='flex flex-col gap-6'>
             <div className='flex flex-col gap-2'>
               <Label htmlFor='title'>Qual a atividade?</Label>
-              <Input id='title' autoFocus placeholder='Correr 5km, meditar...' />
+              <Input id='title' autoFocus placeholder='Correr 5km, meditar...' 
+                {...register('title')}/>
             </div>
+
+            {formState.errors.title?.message && (
+              <p className='text-red-400 text-sm'>
+                {formState.errors.title.message}
+              </p> 
+            )}
             <div className='flex flex-col gap-5'>
               <Label htmlFor='times'>Quantas vezes por semana?</Label>
-              <RadioGroup>
-                <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2'>
-                  <RadioGroupItem value="1" id="option-one" />
-                  <span>1x na semana</span>
-                  <span>🥱</span>
-                </div>
-                <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2'>
-                  <RadioGroupItem value="2" id="option-two" />
-                  <span>2x na semana</span>
-                  <span>🙂</span>
-                </div>
-                <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2'>
-                  <RadioGroupItem value="3" id="option-three" />
-                  <span>3x na semana</span>
-                  <span>😎</span>
-                </div>
-                <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2'>
-                  <RadioGroupItem value="4" id="option-four" />
-                  <span>4x na semana</span>
-                  <span>😜</span>
-                </div>
-                <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2'>
-                  <RadioGroupItem value="5" id="option-five" />
-                  <span>5x na semana</span>
-                  <span>🤨</span>
-                </div>
-                <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2'>
-                  <RadioGroupItem value="6" id="option-six" />
-                  <span className='text-zinc-388 text-sm font-medium leading-none'
-                  >6x na semana</span>
-                  <span>🤯</span>
-                </div>
-                <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2 '>
-                  <RadioGroupItem value="6" id="option-six" />
-                  <span className='text-zinc-388 text-sm font-medium leading-none'
-                  >Todos os dias da semana</span>
-                  <span>🔥</span>
-                </div>
-              </RadioGroup>
+              <Controller
+                control={control}
+                name='weeklyFrequency'
+                render={({field}) => {
+                  return (
+                    <RadioGroup onValueChange={field.onChange} value={String(field.value)} >
+                      <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2'>
+                        <RadioGroupItem value="1" id="option-one" />
+                        <span>1x na semana</span>
+                        <span>🥱</span>
+                      </div>
+                      <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2'>
+                        <RadioGroupItem value="2" id="option-two" />
+                        <span>2x na semana</span>
+                        <span>🙂</span>
+                      </div>
+                      <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2'>
+                        <RadioGroupItem value="3" id="option-three" />
+                        <span>3x na semana</span>
+                        <span>😎</span>
+                      </div>
+                      <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2'>
+                        <RadioGroupItem value="4" id="option-four" />
+                        <span>4x na semana</span>
+                        <span>😜</span>
+                      </div>
+                      <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2'>
+                        <RadioGroupItem value="5" id="option-five" />
+                        <span>5x na semana</span>
+                        <span>🤨</span>
+                      </div>
+                      <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2'>
+                        <RadioGroupItem value="6" id="option-six" />
+                        <span className='text-zinc-388 text-sm font-medium leading-none'
+                        >6x na semana</span>
+                        <span>🤯</span>
+                      </div>
+                      <div className='flex items-center gap-2 bg-black justify-between py-2 px-2 rounded-xl hover:bg-purple-400/80 border-purple-500 border hover:border-white hover:border-2 '>
+                        <RadioGroupItem value="6" id="option-six" />
+                        <span className='text-zinc-388 text-sm font-medium leading-none'
+                        >Todos os dias da semana</span>
+                        <span>🔥</span>
+                      </div>
+                    </RadioGroup>
+                  );
+                }}/>
             </div>
           </div>
 
@@ -80,7 +113,6 @@ export function CreateGoal() {
             <Button className='flex-1' variant='primary'>Salvar</Button>
           </div>
         </form>
-
 
       </div>
     </SheetContent></>
