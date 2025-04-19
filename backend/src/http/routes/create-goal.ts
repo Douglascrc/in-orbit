9f11d579c9230d18897d0f3ff7f533c5a4c4ae10
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2025-04-19 16:44:38 -0300

    refactor: Improve createGoal route

--- backend/src/http/routes/create-goal.ts
+++ backend/src/http/routes/create-goal.ts
@@ -2,20 +2,24 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 import { createGoal } from '../../features/create-goal';
 import z from 'zod';
 
-export const createGoalRoute:FastifyPluginAsyncZod = async(server) => {
-  server.post('/goals', {
-    schema: {
-      body: z.object({
-        title: z.string(),
-        weeklyFrequency: z.number().int().min(1).max(7),
-      }),
+export const createGoalRoute: FastifyPluginAsyncZod = async (server) => {
+  server.post(
+    '/goals',
+    {
+      schema: {
+        body: z.object({
+          title: z.string(),
+          weeklyFrequency: z.number().int().min(1).max(7),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { title, weeklyFrequency } = request.body;
+      const { goal } = await createGoal({
+        title,
+        weeklyFrequency,
+      });
+      reply.code(201).send({ goal });
     }
-  },
-  async (request) => {
-    const { title, weeklyFrequency } = request.body;
-    await createGoal({
-      title,
-      weeklyFrequency,
-    });
-  });
+  );
 };
--- backend/src/http/server.ts
+++ backend/src/http/server.ts
@@ -1,5 +1,9 @@
 import fastify from 'fastify';
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
 import { getWeekPendingGoalsRoute } from './routes/get-week-pending-goals';
 import { createGoalCompletionRoute } from './routes/create-goal-completion';
 import { createGoalRoute } from './routes/create-goal';
@@ -7,14 +11,12 @@ import { getWeekSummaryRoute } from './routes/get-week-summary';
 import cors from '@fastify/cors';
 import { deleteGoalRoute } from './routes/delete-goal';
 
-
 export const server = fastify().withTypeProvider<ZodTypeProvider>();
 
 server.register(cors, {
-  origin:'*',
+  origin: '*',
 });
 
-// Add schema validator and serializer
 server.setValidatorCompiler(validatorCompiler);
 server.setSerializerCompiler(serializerCompiler);
 
@@ -24,8 +26,10 @@ server.register(createGoalRoute);
 server.register(createGoalCompletionRoute);
 server.register(deleteGoalRoute);
 
-server.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP server is running!');
-});
+server
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server is running!');
+  });
